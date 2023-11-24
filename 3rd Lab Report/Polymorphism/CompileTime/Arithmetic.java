273e23696f2cff754ec518fdf7e6616591f29aa7
SOHEL RANA <sohelrana104322@gmail.com>
2023-11-25 02:56:59 +0600

    add:Compile time polymorphism

---
+++ 3rd Lab Report/Polymorphism/CompileTime/Arithmetic.java
@@ -0,0 +1,14 @@
+public class Arithmetic {
+    double sum() {
+        System.out.println("Nothing to sum");
+        return 0;
+    }
+
+    double sum(double a, double b) {
+        return a + b;
+    }
+
+    double sum(double a, double b, double c) {
+        return a + b + c;
+    }
+}
